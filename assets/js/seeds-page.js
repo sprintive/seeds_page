909b01cc3bc713bd3dbfd61d47fcd8ef4968df4e
Syf <syfb96@gmail.com>
2020-11-09 18:35:44 +0200

    SPUTIL-128 Fixed Seeds Modal JS, hide modal contextual link because the modal is apended after JS libraries attached to HTML

--- assets/js/seeds-page.js
+++ assets/js/seeds-page.js
@@ -1,37 +1,37 @@
 (function($, Drupal) {
-  Drupal.behaviors.seedsPage = {
-    attach: function(context, settings) {
-      $(".block-content--type-seeds-modal .field--name-field-seeds-text")
-        .once("seedsPage")
-        .click(function() {
-          var paragraph = $(this)
-            .parent()
-            .clone();
-          paragraph.find(".field--name-field-seeds-text").remove();
-          var modalWrapper = $('<div id="paragraph-modal"></div>');
-          $("body").append(modalWrapper);
-          modalWrapper.html(paragraph.clone());
-          var myDialog = Drupal.dialog(modalWrapper, {
-            resizable: false,
-            title: "",
-            width: "auto",
-            close: function() {
-              $(this).dialog("destroy");
-              modalWrapper.remove();
-            }
-          });
-          myDialog.showModal();
-          modalWrapper.find(".modal-footer").remove();
-        });
+    Drupal.behaviors.seedsPage = {
+        attach: function(context, settings) {
+            $(".block-content--type-seeds-modal .field--name-field-seeds-text")
+                .once("seedsPage")
+                .click(function() {
+                    var paragraph = $(this)
+                        .parent()
+                        .clone();
+                    paragraph.find(".btn-primary.field--name-field-seeds-text").remove();
+                    var modalWrapper = $('<div id="paragraph-modal"></div>');
+                    $("body").append(modalWrapper);
+                    modalWrapper.html(paragraph.clone());
+                    var myDialog = Drupal.dialog(modalWrapper, {
+                        title: "",
+                        width: "auto",
+                        dialogClass: 'seeds-modal-dialog container',
+                        close: function() {
+                            $(this).dialog("destroy");
+                            modalWrapper.remove();
+                        }
+                    });
+                    myDialog.showModal();
+                    modalWrapper.find(".modal-footer").remove();
+                });
 
-      $(".paragraph--view-mode-accordion .title-wrapper").on(
-        "click",
-        function() {
-          $(this)
-            .next()
-            .slideToggle();
+            $(".paragraph--view-mode-accordion .title-wrapper").on(
+                "click",
+                function() {
+                    $(this)
+                        .next()
+                        .slideToggle();
+                }
+            );
         }
-      );
-    }
-  };
+    };
 })(jQuery, Drupal);
