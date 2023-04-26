2509117aba534726a190c24a5c6fef84f47aa102
Yahya Ahmad <yahya@sprintive.com>
2023-04-26 14:46:31 +0300

    Support Druapl 10 and drop drupal 8

--- assets/js/seeds-page.js
+++ assets/js/seeds-page.js
@@ -1,30 +1,31 @@
-(function($, Drupal) {
+(function ($, Drupal, once) {
   Drupal.behaviors.seedsPage = {
-    attach: function(context, settings) {
-      $(".block-content--type-seeds-modal .field--name-field-seeds-text")
-          .once("seedsPage")
-          .click(function() {
-            var paragraph = $(this)
-                .parent()
-                .clone();
-            paragraph.find(".btn-primary.field--name-field-seeds-text").remove();
-            var modalWrapper = $('<div id="paragraph-modal"></div>');
-            $("body").append(modalWrapper);
-            modalWrapper.html(paragraph.clone());
-            var myDialog = modalWrapper.dialog( {
-              autoOpen: true,
-              modal: true,
-              draggable: false,
-              title: "",
-              width: "auto",
-              dialogClass: 'seeds-modal-dialog container',
-              close: function() {
-                $(this).dialog("destroy");
-                modalWrapper.remove();
-              }
-            });
-            modalWrapper.find(".modal-footer").remove();
-          });
-    }
+    attach: function (context, settings) {
+      $(
+        once(
+          "seedsPage",
+          ".block-content--type-seeds-modal .field--name-field-seeds-text"
+        )
+      ).click(function () {
+        var paragraph = $(this).parent().clone();
+        paragraph.find(".btn-primary.field--name-field-seeds-text").remove();
+        var modalWrapper = $('<div id="paragraph-modal"></div>');
+        $("body").append(modalWrapper);
+        modalWrapper.html(paragraph.clone());
+        var myDialog = modalWrapper.dialog({
+          autoOpen: true,
+          modal: true,
+          draggable: false,
+          title: "",
+          width: "auto",
+          dialogClass: "seeds-modal-dialog container",
+          close: function () {
+            $(this).dialog("destroy");
+            modalWrapper.remove();
+          },
+        });
+        modalWrapper.find(".modal-footer").remove();
+      });
+    },
   };
-})(jQuery, Drupal);
+})(jQuery, Drupal, once);
--- assets/js/seeds-tabs.js
+++ assets/js/seeds-tabs.js
@@ -1,28 +1,36 @@
-(function($, Drupal) {
+(function ($, Drupal, once) {
   Drupal.behaviors.seedsTabs = {
-    attach: function(context, settings) {
-      $(".seeds-paragraph-block .tabs>.field--item:eq(0)").addClass("seeds-expand");
-	  // Turn tabs into a slider.
-	  $(".seeds-paragraph-block .tabs-wrapper").slick({
-		variableWidth: true,
-		infinite: false,
-		arrows:false,
-		swipeToSlide: true,
-		draggable: true,
-		swipe: true,
-		touchThreshold: 20,
-    touchMove: true,
-    rtl: $('html').attr('dir') == 'rtl'?true:false
-	  });
-	$()
-      $(".seeds-paragraph-block .tabs .tabs-wrapper li").once('seedsTabs').each(function() {
-        $(this).click(function() {
-          $(this).parents(".tabs").find(">.field--item").removeClass("seeds-expand");
-          $(this).parents(".tabs").find(`>.field--item:eq("${$(this).index()}")`).addClass(
-            "seeds-expand"
-          );
+    attach: function (context, settings) {
+      $(".seeds-paragraph-block .tabs>.field--item:eq(0)").addClass(
+        "seeds-expand"
+      );
+      // Turn tabs into a slider.
+      $(".seeds-paragraph-block .tabs-wrapper").slick({
+        variableWidth: true,
+        infinite: false,
+        arrows: false,
+        swipeToSlide: true,
+        draggable: true,
+        swipe: true,
+        touchThreshold: 20,
+        touchMove: true,
+        rtl: $("html").attr("dir") == "rtl" ? true : false,
+      });
+      $();
+      $(
+        once("seedsTabs", ".seeds-paragraph-block .tabs .tabs-wrapper li")
+      ).each(function () {
+        $(this).click(function () {
+          $(this)
+            .parents(".tabs")
+            .find(">.field--item")
+            .removeClass("seeds-expand");
+          $(this)
+            .parents(".tabs")
+            .find(`>.field--item:eq("${$(this).index()}")`)
+            .addClass("seeds-expand");
         });
       });
-    }
+    },
   };
-})(jQuery, Drupal);
+})(jQuery, Drupal, once);
