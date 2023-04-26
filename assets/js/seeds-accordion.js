235fb763e2e0a603ebf92a0dd5c02381260fdd19
Yahya Ahmad <yahya@sprintive.com>
2023-04-26 14:49:25 +0300

    Support Druapl 10 and drop drupal 8

--- assets/js/seeds-accordion.js
+++ assets/js/seeds-accordion.js
@@ -1,15 +1,11 @@
-(function($, Drupal) {
-    Drupal.behaviors.seedsAccordion = {
-        attach: function(context, settings) {
-            $(".paragraph--view-mode-accordion .title-wrapper").once('seedsAccordion').on(
-                "click",
-                function() {
-                    $(this)
-                        .toggleClass('active')
-                        .next()
-                        .slideToggle();
-                }
-            );
-        }
-    };
-})(jQuery, Drupal);
+(function ($, Drupal, once) {
+  Drupal.behaviors.seedsAccordion = {
+    attach: function (context, settings) {
+      $(
+        once("seedsAccordion", ".paragraph--view-mode-accordion .title-wrapper")
+      ).on("click", function () {
+        $(this).toggleClass("active").next().slideToggle();
+      });
+    },
+  };
+})(jQuery, Drupal, once);
